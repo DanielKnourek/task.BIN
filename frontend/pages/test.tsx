91a643f3106d5b0012dc1f32dd16910b06d7a3de
DanielKnourek <dev.danielknourek@gmail.com>
2021-11-01 18:27:43 +0100

    workspace v0.3 clean

---
+++ frontend/pages/test.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next"
+
+
+const Home: NextPage = () => {
+    return (
+        <h1>Test page 002</h1>
+    )
+}
+
+export default Home
---
+++ frontend/pages/test2.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next"
+
+
+const Home: NextPage = () => {
+    return (
+        <h1>Test 2 page 002</h1>
+    )
+}
+
+export default Home
