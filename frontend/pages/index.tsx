be27e1e5d8ba262750ea89ecf9ce5cc85177057c
DanielKnourek <dev.danielknourek@gmail.com>
2021-12-21 03:45:17 +0100

    dev test chages

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -4,7 +4,7 @@ const Demo: NextPage = () => {
 
     return (
         <>
-        <div className="text-center text-green-700 font-bold bg-blue-400">
+        <div className="text-center text-green-700 font-bold bg-blue-400 left-5">
             Buatiful block 13
         </div>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
