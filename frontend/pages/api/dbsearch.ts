aeadab8ae332f720639f0b1b5cec001b0cfc67d1
DanielKnourek <dev.danielknourek@gmail.com>
2021-12-22 07:31:55 +0100

    Test with RequestParams checking

--- frontend/pages/api/dbsearch.ts
+++ frontend/pages/api/dbsearch.ts
@@ -4,6 +4,7 @@ import Client_meredith from '../../lib/db/Client_meredith'
 import { Task, IDlessTask } from '../../types/interfaces/Task'
 import { RequestParams } from '@elastic/elasticsearch'
 import { SearchResponse } from '../../types/interfaces/ElasticSearch'
+import { Search } from '@elastic/elasticsearch/api/requestParams'
 
 const test = async (
     req: NextApiRequest,
@@ -15,19 +16,22 @@ const test = async (
         }
     }
 
-    Client_meredith.search<SearchResponse<IDlessTask>, SearchBody>({
-        index: 'task',
+    const params: Search<SearchBody> = {
+        index: "task",
         body: {
             query: {
-                match: { ownerID: "daniel" }
+                match: { ownerID: "Daniel" }
             }
         }
-    }).then( ({ body, statusCode, headers, warnings, meta }) => {
-        if( statusCode != 200) { throw new Error("Request to database was unsuccessful."); }
+    } 
 
+    Client_meredith.search<SearchResponse<IDlessTask>, SearchBody>(params
+    ).then( ({ body, statusCode, headers, warnings, meta }) => {
+        if( statusCode != 200) { throw new Error("Request to database was unsuccessful."); }
+        
         let result: Task[] = body.hits.hits.map(hit => { return { id: hit._id, ...hit._source } })
         res.send(JSON.stringify(result, null, 2));
-        
+
     }).catch( reason => {
         res.status(503).send(reason.toString());
     })
