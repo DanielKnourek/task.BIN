4b07fb899ece6c6e3b84698d5fa57e8b598fef29
DanielKnourek <dev.danielknourek@gmail.com>
2022-01-07 02:33:24 +0100

    V1.0 changes

--- frontend/pages/my.tsx
+++ frontend/pages/my.tsx
@@ -3,24 +3,24 @@ import Head from 'next/head'
 import TaskList from '../components/task/TaskList';
 import SideBar from '../components/sideBar';
 
-const PageHome: NextPage = () => {
+const PageMy: NextPage = () => {
 
     return (
         <div
             className="flex flex-col min-h-screen bg-gray-200"
         >
             <Head>
-                <title>Global tasks - task.BIN</title>
+                <title>My tasks - task.BIN</title>
                 <html lang='en'/>
             </Head>
             <SideBar />
             
             <div className="antialiased text-gray-900 font-sans p-4">
-                <TaskList access='public' />
+                <TaskList access='private' />
             </div>
             <div className='flex-grow'></div>
         </div>
     )
 }
 
-export default PageHome;
+export default PageMy;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,21 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blueGray: colors.blueGray,
+        violet: colors.violet,
+        lime: colors.lime,
+        'violet-xl': '#311362',
+      },
+      translate: {
+        'fullminus': '10rem',
+      },
+    },
   },
   variants: {
     extend: {},
